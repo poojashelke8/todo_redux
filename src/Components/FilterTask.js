920635ad80c50a59c72ab5551cf09e97b322059f
poojashelke8 <poojabshelke0807@gmail.com>
2024-06-11 16:40:22 +0530

    added new styles and components

--- src/Components/FilterTask.js
+++ src/Components/FilterTask.js
@@ -3,7 +3,14 @@ import "../Components/FilterTask.css"
 
 const FilterTask = () => {
   return (
-    <div className='filterTask'>FilterTask</div>
+    <div className='filterTask'>
+      <h1 id='filtersheading'>Filters</h1>
+      <div className='filterlist'>
+        <li className='filterlist1'>Starred</li>
+        <li className='filterlist1'>Week</li>
+        <li className='filterlist1'>Important</li>
+      </div>
+    </div>
   )
 }
 
--- src/Components/TaskMain.js
+++ src/Components/TaskMain.js
@@ -5,10 +5,12 @@ import "../Components/Taskmain.css"
 
 const TaskMain = () => {
   return (
-    <div className='taskmain'>
-        {/* <h1>TODO</h1> */}
+    <div className='main'>
+    <h1 id='taskheading'>todo.</h1>
+    <div className='taskmain1'>
         <FilterTask/>
         <TaskInput/>
+    </div>
     </div>
   )
 }
